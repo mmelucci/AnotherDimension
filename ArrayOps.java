edcbd5defa4009c7818e77c186f26ebf98759b2a
mmelucci <mmelucci20@stuy.edu>
2020-10-24 15:34:17 -0400

    I completed largest() and its test cases

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,14 @@ public class ArrayOps {
     }
     return sum;
   }
+
+  public static  int largest(int[] arr) {
+    int largest = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -5,12 +5,22 @@ public class Tester {
     int[] array2 = {1, 2};
     int[] array3 = {1, 2, 3};
     int[] array4 = {1, 2, 3, 4};
-    int[] array5 = {1, 2, 3, 4, 5};
+    int[] array5 = {4, 1, 5, 2, 3};
+
+    System.out.println("sum() Tests:");
     System.out.println(ArrayOps.sum(array0));
     System.out.println(ArrayOps.sum(array1));
     System.out.println(ArrayOps.sum(array2));
     System.out.println(ArrayOps.sum(array3));
     System.out.println(ArrayOps.sum(array4));
     System.out.println(ArrayOps.sum(array5));
-}
+    System.out.println();
+    System.out.println("largest() Tests:");
+    System.out.println(ArrayOps.largest(array0));
+    System.out.println(ArrayOps.largest(array1));
+    System.out.println(ArrayOps.largest(array2));
+    System.out.println(ArrayOps.largest(array3));
+    System.out.println(ArrayOps.largest(array4));
+    System.out.println(ArrayOps.largest(array5));
+  }
 }
