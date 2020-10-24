3f22f280aecde960f15a596dd135936170e8e8cb
mmelucci <mmelucci20@stuy.edu>
2020-10-24 15:21:09 -0400

    I wrote test cases for the sum() method

---
+++ Tester.java
@@ -0,0 +1,16 @@
+public class Tester {
+  public static void main(String[] args) {
+    int[] array0 = {};
+    int[] array1 = {1};
+    int[] array2 = {1, 2};
+    int[] array3 = {1, 2, 3};
+    int[] array4 = {1, 2, 3, 4};
+    int[] array5 = {1, 2, 3, 4, 5};
+    System.out.println(ArrayOps.sum(array0));
+    System.out.println(ArrayOps.sum(array1));
+    System.out.println(ArrayOps.sum(array2));
+    System.out.println(ArrayOps.sum(array3));
+    System.out.println(ArrayOps.sum(array4));
+    System.out.println(ArrayOps.sum(array5));
+}
+}
